45cea6c23edeef1f9fa0eebb8b17b93c1e00c9de
Max Rauchman <max.l.rauchman@gmail.com>
2023-06-16 10:58:05 -0400

    UPdated Preorder

--- algos/data_structures/BinarySearchTree/index.js
+++ algos/data_structures/BinarySearchTree/index.js
@@ -87,6 +87,13 @@ class BinarySearchTree {
         return vals;
     }
 
+    toArrPreorder2(node = this.root, vals = []) {
+        if(!node) { return vals };
+        vals.push(node.data);
+        this.toArrPreorder(node.left, vals);
+        this.toArrPreorder(node.right, vals);
+    }
+
     /**
      * DFS Inorder: (Left, CurrNode, Right)
      * Converts this BST into an array following Depth First Search inorder.
